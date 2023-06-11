51194db5f53d66c1d3379ec8b6a3b13ac242afff
jesuisstan <stan.krivtsov@gmail.com>
2023-06-11 14:08:35 +0200

    App_started

---
+++ omeo-search-bar/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </div>
+  );
+}
+
+export default App;
